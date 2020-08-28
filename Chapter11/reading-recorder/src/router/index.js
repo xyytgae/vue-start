c1454c01ad7027f30701bdeab1d4a94268bfaa7b
xyytgae <xyytgae@yahoo.co.jp>
2020-08-28 20:53:22 +0900

    Create Chapter11

---
+++ Chapter11/reading-recorder/src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BookSearch from '../components/BookSearch.vue'
+import BookForm from '../components/BookForm.vue'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/search',
+    name: 'Search',
+    component: BookSearch,
+  },
+  {
+    path: '/form',
+    name: 'Form',
+    component: BookForm,
+  },
+  {
+    path: '*',
+    redirect: '/',
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+})
+
+export default router
